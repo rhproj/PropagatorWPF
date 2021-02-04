4fa15144ecc4df0d947ecb6ce7e6b7e174914d9d
rh <rh@mail.com>
2021-02-04 12:30:58 +0300

    1 Arm case

--- PropagatorWPF/MainWindow.xaml.cs
+++ PropagatorWPF/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,76 @@ namespace PropagatorWPF
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        DirectoryInfo resoursePath = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory + "Resources");
+        IList<string> resourseList = new List<string>();
+        int countResItems;
+
+        DirectoryInfo copyTo = new DirectoryInfo(Environment.GetFolderPath(Environment.SpecialFolder.Desktop)); //Environment.GetFolderPath() !! don;t forget otherwise it roots from Debug..
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (resoursePath.Exists)
+            {
+                countResItems = ListAll(resoursePath, resourseList);
+                
+                //lvArm.Items.Add(resoursePath);
+            }
+            else
+            {
+                resourseList.Add("Проверьте наличие папки 'Resources'");
+            }
+
+            lvArm.ItemsSource = resourseList;
+            tBlCount.Text = countResItems.ToString();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (countResItems > 0)
+            {
+                CopyAll(resoursePath, copyTo);
+                MessageBox.Show("Готово!");
+            }
+            else
+            {
+                MessageBox.Show("Папка ресурсов пуста!");
+                Application.Current.Shutdown();
+            }
+        }
+
+        private void CopyAll(DirectoryInfo fromD, DirectoryInfo toD)
+        {
+            //Directory.CreateDirectory(toD.FullName); //don't need it in case of Desktop
+            //copy files
+            foreach (FileInfo fI in fromD.GetFiles())
+            {
+                fI.CopyTo(System.IO.Path.Combine(toD.FullName, fI.Name), true);
+            }
+            //copy sub-dirs recursively
+            foreach (DirectoryInfo sourceDirs in fromD.GetDirectories())
+            {
+                DirectoryInfo targDirs = toD.CreateSubdirectory(sourceDirs.Name);
+                CopyAll(sourceDirs, targDirs);
+            }
+        }
+
+        private int ListAll(DirectoryInfo fromD, IList<string> resourseList)
+        {
+            foreach (FileInfo fI in fromD.GetFiles())
+            {
+                resourseList.Add(fI.Name);
+            }
+
+            //copy sub-dirs recursively
+            foreach (DirectoryInfo sourceDirs in fromD.GetDirectories())
+            {
+                resourseList.Add(sourceDirs.Name);
+                //ListAll(sourceDirs, resourseList);
+            }
+
+            return resourseList.Count();
         }
     }
 }
